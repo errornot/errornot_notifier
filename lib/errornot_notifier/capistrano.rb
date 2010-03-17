df4a3612043b2ba6af19dbd701b49003871d9bc2
Cyril Mougel <cyril.mougel@gmail.com>
2010-03-17 13:09:47 +0100

    Comment capistrano task not be use now

--- lib/errornot_notifier/capistrano.rb
+++ lib/errornot_notifier/capistrano.rb
@@ -7,13 +7,14 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deploy do
     desc "Notify Errornot of the deployment"
     task :notify_errornot, :except => { :no_release => true } do
-      rails_env = fetch(:errornot_env, fetch(:rails_env, "production"))
-      local_user = ENV['USER'] || ENV['USERNAME']
-      executable = RUBY_PLATFORM.downcase.include?('mswin') ? 'rake.bat' : 'rake'
-      notify_command = "#{executable} errornot:deploy TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
-      notify_command << " API_KEY=#{ENV['API_KEY']}" if ENV['API_KEY']
-      puts "Notifying Errornot of Deploy (#{notify_command})"
-      `#{notify_command}`
+      # TODO : reactivate it when rake deploy define
+      # rails_env = fetch(:errornot_env, fetch(:rails_env, "production"))
+      # local_user = ENV['USER'] || ENV['USERNAME']
+      # executable = RUBY_PLATFORM.downcase.include?('mswin') ? 'rake.bat' : 'rake'
+      # notify_command = "#{executable} errornot:deploy TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
+      # notify_command << " API_KEY=#{ENV['API_KEY']}" if ENV['API_KEY']
+      # puts "Notifying Errornot of Deploy (#{notify_command})"
+      # `#{notify_command}`
       puts "Errornot Notification Complete."
     end
   end
