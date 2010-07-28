5d53c9d049557aa07a7630d3ca966a4f82668cb2
Cyril Mougel <cyril.mougel@gmail.com>
2010-07-28 21:47:44 +0200

    railtie_name is deprecated
    Conflicts:
    
    	lib/errornot_notifier/railtie.rb

--- lib/errornot_notifier/railtie.rb
+++ lib/errornot_notifier/railtie.rb
@@ -3,8 +3,6 @@ require 'rails'
 
 module ErrornotNotifier
   class Railtie < Rails::Railtie
-    railtie_name :errornot_notifier
-
     rake_tasks do
       require "errornot_notifier/rails3_tasks"
     end
